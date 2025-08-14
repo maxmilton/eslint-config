bf908302020d0c012d6e4044a8189758171b9f84
Max Milton <max@happyhackers.xyz>
2025-08-14 19:02:05 +0900

    chore: Tweak linter configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import eslint from '@eslint/js';
+import js from '@eslint/js';
 import unicorn from 'eslint-plugin-unicorn';
 import ts from 'typescript-eslint';
 import mm from './src/index.js';
 
 export default ts.config(
-  eslint.configs.recommended,
-  ...ts.configs.strictTypeChecked,
-  ...ts.configs.stylisticTypeChecked,
+  js.configs.recommended,
+  ts.configs.strictTypeChecked,
+  ts.configs.stylisticTypeChecked,
   unicorn.configs.recommended,
   mm.configs.recommended,
   {
